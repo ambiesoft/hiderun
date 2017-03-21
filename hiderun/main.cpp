aada8c755cc6eca7ec36ac0aa09c186fbe1d5440
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-03-21 00:30:51 +0000

    Add 2017 solution
    git-svn-id: svn://192.168.3.99/hiderun@6873 5ada3c48-002d-0410-850d-99865cc6fd0f

--- hiderun/main.cpp
+++ hiderun/main.cpp
@@ -1,5 +1,8 @@
 #include <windows.h>
+
+
 #include "../../MyUtility/CreateProcessCommon.h"
+#include "../../MyUtility/Is64.h"
 
 #define APPNAME L"hiderun"
 
@@ -15,22 +18,50 @@ void* memset(void* dist, int val, size_t size)
 }
 
 
-void entrypoint()
+int main()
 {
 	int argc=0;
 	LPWSTR* argv=CommandLineToArgvW(GetCommandLine(), &argc);
 	if(argc<2)
 	{
+		MessageBox(NULL, L"No Arguments", APPNAME, MB_ICONEXCLAMATION);
 		LocalFree(argv);
-		ExitProcess(1);
+		return(1);
 	}
+
+	if (Is64BitWindows() && !Is64BitProcess())
+	{
+		TCHAR szT[MAX_PATH]; szT[0] = 0;
+		GetModuleFileName(NULL, szT, _countof(szT));
+		size_t len = lstrlen(szT);
+		LPWSTR p = &szT[len] - 1;
+		for (; p != szT; --p)
+		{
+			if (*p == L'\\')
+			{
+				lstrcpy(p+1, L"hiderun64.exe");
+				if (!CreateProcessCommon(szT, argv[1]))
+				{
+					MessageBox(NULL, L"Failed to launch hiderun64", APPNAME, MB_ICONEXCLAMATION);
+					return 1;
+				}
+				return 0;
+			}
+		}
+		return 1;
+	}
+
 	int ret=0;
 	if(!CreateProcessCommon(argv[1],NULL, TRUE))//L"C:\\Linkout\\bin\\curr.bat");
 	{
-		MessageBox(NULL, L"Failed", APPNAME, MB_ICONEXCLAMATION);
+		MessageBox(NULL, L"Failed to launch", APPNAME, MB_ICONEXCLAMATION);
 		ret=1;
 	}
 	LocalFree(argv);
-	ExitProcess(ret);
+	return (ret);
 }
 
+void entrypoint()
+{
+	ExitProcess(main());
+}
